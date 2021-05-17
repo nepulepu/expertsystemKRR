1728f630e1b19413614ee3c9bc113d2a8e69b276
nepulepu <azrixanwar@gmail.com>
2021-05-18 02:19:28 +0800

    commit

--- main.py
+++ main.py
@@ -1,13 +1,33 @@
+from logging import currentframe
 import telebot
 import os
+import time
 from dotenv import load_dotenv
+from datetime import datetime
 
 load_dotenv()
-TOKEN=os.getenv("TOKEN")
-bot=telebot.Telebot(TOKEN)
+TOKEN=os.getenv("TOKEN2")
+print(TOKEN)
+bot=telebot.TeleBot(TOKEN)
 
-@bot.message_handler(commands=['GoodDayDr'])
+def getTime():
+    now= datetime.now()
+    currenttime=now.strftime("%H")
+    if(int(currenttime)<6 or int(currenttime)>=15):
+        return "Evening"
+    elif(int(currenttime)>=6 and int(currenttime)<12):
+        return "Morning"
+    elif(int(currenttime)>=12 and int(currenttime)<15):
+        return "Afternoon"
+
+@bot.message_handler(commands=['HelloDr'])
 def greet(message):
-    bot(message, "Good Day Student\nAre you ready to start the class?")
+    name=message.from_user.first_name
+    bot.send_message(message.chat.id, "Good "+getTime()+" "+name+",\nAre you ready to start the class?")
 
-bot.polling()
+while True:
+    try:
+        bot.polling(none_stop=False)
+    except Exception:
+        print('crash')
+        time.sleep(1)
